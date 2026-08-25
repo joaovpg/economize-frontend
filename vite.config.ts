0a764dd6d5c5402d1ddd90b44e40edbb136d1cbe
João Victor Gomes <60114383+joaovpg@users.noreply.github.com>
2026-08-25 17:02:34 -0300

    chore: enable React Compiler with Oxc

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [tailwindcss(), react({ compiler: true })],
 });
